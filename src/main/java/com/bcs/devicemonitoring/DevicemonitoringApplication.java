7cfd8b91abe82198f04dc2859420ed54454e8c85
Nelson Kimaiga <nelsonkimaiga@aol.com>
2026-06-03 20:13:21 +0300

    first commit, with settings and sources set up for scaffolding

---
+++ src/main/java/com/bcs/devicemonitoring/DevicemonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.bcs.devicemonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevicemonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevicemonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bcs/devicemonitoring/DevicemonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcs.devicemonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevicemonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
